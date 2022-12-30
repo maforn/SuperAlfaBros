81a3d4be30ed4f168910e74fc00c43fe00809b70
Gargantua256 <90470996+Gargantua256@users.noreply.github.com>
2022-12-30 16:39:46 +0100

    Gravity, jump and rendering

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
-//
-// Created by matteo on 23/11/2022.
-//
 #include "Lists/DynamicLevelList.hpp"
+#include <chrono>
+#include <cmath>
+
+
+inline double CurrentTime_milliseconds()
+{
+    return chrono::duration_cast<std::chrono::milliseconds>
+            (chrono::high_resolution_clock::now().time_since_epoch()).count();
+}
 
 int main() {
     // create a new player instance
@@ -20,6 +26,10 @@ int main() {
     width = 90;
     start_y = 0;
     start_x = 0;
+
+    const double TEMPO = 500;
+    const double MINTEMPO = 40.0;
+    double delay = TEMPO;  //delay as gravity
 
     // hide the blinking cursor
     curs_set(0);
@@ -48,11 +58,24 @@ int main() {
     // refresh the window with the new data drawn
     wrefresh(win);
 
+    cbreak();
+    noecho(); //don't print the keys pressed while playing
+    nodelay(stdscr, TRUE); //make getch not wait for the input
+
+
+    int lastx, lasty;
+    pMap lastMap;
     // detect player moves
     int choice;
+
+    auto start = CurrentTime_milliseconds();
+
     do {
+        lastx = player->x;
+        lasty = player->y;
+        lastMap = levels->currentMap();
         // get choice from keyboard
-        choice = wgetch(win);
+        choice = getch();
         switch (choice) {
             case 'w': // move player forward
                 levels->movePlayer(player->x, player->y - 1);
@@ -66,28 +89,54 @@ int main() {
             case 'd': // move player rightward
                 levels->movePlayer(player->x + 1, player->y);
                 break;
+            case ' ': // jump where and if possible, maximum of 3
+                if(lasty > 1) levels->movePlayer(player->x, player->y-1);   // if within bounds
+                if(player->y==lasty-1 && lasty > 2) levels->movePlayer(player->x, player->y-1); // if previous successful and within bounds
+                if(player->y==lasty-2 && lasty > 3) levels->movePlayer(player->x, player->y-1); // if previous successful and within bounds
+                delay = TEMPO;
+                break;
         }
-        // clear the windows and draw everything again
-        wclear(win);
-        if (player->getLife() <= 0) {
-            break;
+
+        if ((CurrentTime_milliseconds() - start)>delay) // gravity, acts based on time passed
+        {
+            levels->movePlayer(player->x, player->y + 1); // fall
+            start = CurrentTime_milliseconds();
+            if (lasty!=player->y)
+            {
+                delay = max(delay*0.75,MINTEMPO);  // fall faster next time, delay is lower delay/(delay+10.0);
+            }
+            else delay = TEMPO; // reset delay if not falling, no speed
         }
-        levels->currentMap()->drawBaseMap(win);
-        levels->currentMap()->drawObjects(win);
-        player->drawPlayer(win);
-        // refresh the window
-        wrefresh(win);
-    } while (choice != KEY_EXIT);
+
+
+        if (lastx!=player->x || lasty!=player->y || levels->currentMap()!=lastMap) { // if something changes
+
+            if (levels->currentMap()!=lastMap) // if map changes
+            {
+                // clear the window and draw Map and Objects
+                wclear(win);
+                levels->currentMap()->drawBaseMap(win);
+                levels->currentMap()->drawObjects(win);
+            }
+            mvwaddwstr(win, lasty, lastx, L" "); // clear previous coordinate where player was
+            player->drawPlayer(win);
+            // refresh the window
+            wrefresh(win);
+        }
+    } while (choice != 'q' && player->getLife()>0);
+    nodelay(stdscr, FALSE);
+    wclear(win);
     wrefresh(win);
-    addwstr(L"\n\n    ▄██████▄     ▄████████    ▄▄▄▄███▄▄▄▄      ▄████████       ▄██████▄   ▄█    █▄     ▄████████    ▄████████ \n"
-                  "   ███    ███   ███    ███  ▄██▀▀▀███▀▀▀██▄   ███    ███      ███    ███ ███    ███   ███    ███   ███    ███ \n"
-                  "   ███    █▀    ███    ███  ███   ███   ███   ███    █▀       ███    ███ ███    ███   ███    █▀    ███    ███ \n"
-                  "  ▄███          ███    ███  ███   ███   ███  ▄███▄▄▄          ███    ███ ███    ███  ▄███▄▄▄      ▄███▄▄▄▄██▀ \n"
-                  " ▀▀███ ████▄  ▀███████████  ███   ███   ███ ▀▀███▀▀▀          ███    ███ ███    ███ ▀▀███▀▀▀     ▀▀███▀▀▀▀▀   \n"
-                  "   ███    ███   ███    ███  ███   ███   ███   ███    █▄       ███    ███ ███    ███   ███    █▄  ▀███████████ \n"
-                  "   ███    ███   ███    ███  ███   ███   ███   ███    ███      ███    ███ ███    ███   ███    ███   ███    ███ \n"
-                  "   ████████▀    ███    █▀    ▀█   ███   █▀    ██████████       ▀██████▀   ▀██████▀    ██████████   ███    ███ \n"
-                  "                                                                                                   ███    ███ \n");
+    addwstr(L"\n\n"
+            "    ▄██████▄     ▄████████    ▄▄▄▄███▄▄▄▄      ▄████████       ▄██████▄   ▄█    █▄     ▄████████    ▄████████ \n"
+            "   ███    ███   ███    ███  ▄██▀▀▀███▀▀▀██▄   ███    ███      ███    ███ ███    ███   ███    ███   ███    ███ \n"
+            "   ███    █▀    ███    ███  ███   ███   ███   ███    █▀       ███    ███ ███    ███   ███    █▀    ███    ███ \n"
+            "  ▄███          ███    ███  ███   ███   ███  ▄███▄▄▄          ███    ███ ███    ███  ▄███▄▄▄      ▄███▄▄▄▄██▀ \n"
+            " ▀▀███ ████▄  ▀███████████  ███   ███   ███ ▀▀███▀▀▀          ███    ███ ███    ███ ▀▀███▀▀▀     ▀▀███▀▀▀▀▀   \n"
+            "   ███    ███   ███    ███  ███   ███   ███   ███    █▄       ███    ███ ███    ███   ███    █▄  ▀███████████ \n"
+            "   ███    ███   ███    ███  ███   ███   ███   ███    ███      ███    ███ ███    ███   ███    ███   ███    ███ \n"
+            "   ████████▀    ███    █▀    ▀█   ███   █▀    ██████████       ▀██████▀   ▀██████▀    ██████████   ███    ███ \n"
+            "                                                                                                   ███    ███ \n");
     refresh();
     getch();
     endwin();
